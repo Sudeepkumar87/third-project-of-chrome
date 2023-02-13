c999dc3e263780d47dea18b287b1e127c7230c8b
Sudeep <mastersudeepkumar@gmail.com>
2023-02-13 06:19:18 +0530

    third project

---
+++ script.js
@@ -0,0 +1,30 @@
+// this code is for hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", ()=>{
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+// this code is for hamburger menu end here
+const navBar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  if (window.scrollY>100){
+    navBar.classList.add("navActive");
+
+  } else{
+    navBar.classList.remove("navActive")
+  }
+}
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+tabs.forEach(tab =>{
+  tab.addEventListener('click',()=>{
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach(tabContent =>{
+      tabContent.classList.remove('active')
+    })
+    target.classList.add('active')
+  })
+})
